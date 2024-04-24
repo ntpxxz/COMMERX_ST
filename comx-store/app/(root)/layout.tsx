17eb196ab52fac6288a5c4146e0f0ede3b90e43a
ntpxxz <63597391+ntpxxz@users.noreply.github.com>
2024-04-24 14:40:36 +0700

    NEW_STORE

---
+++ comx-store/app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "../globals.css"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Zam Store",
+  description: "Zam store Next 14",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <ClerkProvider>{children}</ClerkProvider>
+      </body>
+    </html>
+  );
+}
